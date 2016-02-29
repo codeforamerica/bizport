6d304518ac4946165c7444c6855631de18389d35
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-02-29 11:14:41 -0800

    Partway through uploading copy for initial pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,16 @@
 class StaticController < ApplicationController
   def home
   end
+
+  def business_plan
+  end
+
+  def find_a_location
+  end
+
+  def find_a_location
+  end
+
+  def choose_a_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
+  namespace :start do
+    get 'business-plan' => '/static#business_plan'
+    get 'finance-your-business' => '/static#finance_your_business'
+    get 'find-a-location' => '/static#find_a_location'
+    get 'choose-a-name' => '/static#choose_a_name'
+    get 'register-your-business' => '/static#register_your_business'
+  end
+
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
