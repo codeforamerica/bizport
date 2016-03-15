6e32c93a289c6f5a45727956fb8b81dce5006c7e
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-03-15 13:31:29 -0700

    Don't load Puma settings for now while working with Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,15 @@
-directory "/var/www/bizport/current"
-environment 'production'
-workers 1
-threads 1, 8
+# TODO: this should actually read Rails.env.production?
+# this needs to be set up such that it can deploy on city server and Heroku equally smoothly
+if false 
+  directory "/var/www/bizport/current"
+  environment 'production'
+  workers 1
+  threads 1, 8
 
-bind 'tcp://0.0.0.0:9951'
-pidfile "/var/www/bizport/current/tmp/puma/pid"
-state_path "/var/www/bizport/current/tmp/puma/state"
+  bind 'tcp://0.0.0.0:9951'
+  pidfile "/var/www/bizport/current/tmp/puma/pid"
+  state_path "/var/www/bizport/current/tmp/puma/state"
 
-preload_app!
-activate_control_app
+  preload_app!
+  activate_control_app
+end
