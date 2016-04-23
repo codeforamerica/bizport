1f77155f5942a6c207273b40477ffc551384281d
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-22 17:32:01 -0700

    Remove readmore after-collapse scroll effect

--- app/assets/javascripts/readmore-config.js
+++ app/assets/javascripts/readmore-config.js
@@ -18,11 +18,5 @@ $(function(){
         $(element).removeClass('readmore-expanded');
       }
     },
-
-    afterToggle: function(trigger, element, expanded) {
-      if(! expanded) { // The "Close" link was clicked
-        $('html, body').animate( { scrollTop: element.offset().top }, {duration: 250 } );
-      }
-    },
   });
 });
