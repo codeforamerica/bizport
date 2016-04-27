3c323f2f191d69265553c3ba65c6f00cf75a1622
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-27 15:52:34 -0700

    More pages built in CMS fixtures. Nav rendered from existing pages.

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,6 @@ Rails.application.routes.draw do
 
   get '/resources' => 'static#resources'
 
-  namespace :start do
-    get 'business-plan' => '/static#business_plan'
-    get 'finance-your-business' => '/static#finance_your_business'
-    get 'find-a-location' => '/static#find_a_location'
-    get 'register-your-business' => '/static#register_your_business'
-    get 'permits-and-licenses' => '/static#permits_and_licenses'
-    get 'building-and-inspections' => '/static#building_and_inspections'
-    get 'open-your-business' => '/static#open_your_business'
-  end
-
   comfy_route :cms_admin, :path => '/admin'
   comfy_route :cms, :path => '/', :sitemap => false # Make sure this routeset is defined last
 
