cb0501a0d4df13aa27f94a03d791f540a713cadf
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-28 10:27:59 -0700

    Symlink common folders for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ after "deploy:updated", "newrelic:notice_deployment"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
