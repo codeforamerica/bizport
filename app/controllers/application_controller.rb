d1481b9426152ae34d073e81f0c87e2134af3cc0
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-04-28 17:15:58 -0700

    Refactored "next" button into template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include Comfy::CmsHelper
+  # used in CMS "step" pages to set up "next page" links
+  def next_step_page(component)
+    page = Comfy::Cms::Page.find_by(full_path: request.path)
+    next_page = Comfy::Cms::Page.find_by(position: page.position+1)
+    case component
+    when "title"
+      return "Step #{next_page.position}: #{next_page.label}"
+    when "path"
+      return next_page.full_path
+    end
+  end
+  helper_method :next_step_page
 end
