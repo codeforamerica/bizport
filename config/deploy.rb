ef9359e540f2b55f4d108bc2d51140b528bad6d5
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-05-13 11:16:35 -0700

    Moved sitemap to shared/ folder for continuity across deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ after "deploy:updated", "newrelic:notice_deployment"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'public/sitemap.xml.gz')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
