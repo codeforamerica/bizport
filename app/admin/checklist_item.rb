ebd661083334fc9c0070c593275332ee86fd9e84
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-05-13 08:44:32 -0700

    Added CRUD in Admin views

--- app/admin/checklist_item.rb
+++ app/admin/checklist_item.rb
@@ -2,16 +2,6 @@ ActiveAdmin.register ChecklistItem do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  permit_params :item_text, :category, :category_name, :order_number
 
 end
--- app/admin/glossary_item.rb
+++ app/admin/glossary_item.rb
@@ -2,15 +2,6 @@ ActiveAdmin.register GlossaryItem do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-permit_params do
-  permitted = [:term, :definition]
-  permitted
-end
-
+  permit_params :term, :definition
 
 end
