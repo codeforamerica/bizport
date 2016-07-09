7275f55a9a3a5c88ecae1f55078e143b85e7608c
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-07-08 18:04:19 -0700

    Added routes, views, and controllers for /resources/search create and show

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,33 @@
+class ResourcesController < ApplicationController
+  require 'net/http'
+
+  API_URL = 'http://54.153.4.226/wm-api/'
+
+  def new
+  end
+
+  # POST
+  def search
+    search_params = params
+    url = URI.parse(API_URL + 'search/')
+    res = Net::HTTP.post_form(url, params)
+    @api_response = JSON.parse(res.body)
+
+    redirect_to '/resources/search/'+@api_response['id'].to_s
+  end
+
+  def results
+    search_id = params[:id]
+    url = URI.parse(API_URL+'search/'+search_id+'/')
+    res = Net::HTTP.get_response(url)
+    @results = JSON.parse(res.body)
+  end
+
+  def show
+    opportunity_id = params[:id]
+    url = URI.parse(API_URL+opportunity_id+'/')
+    res = Net::HTTP.get(url)
+    @opportunity = JSON.parse(res.body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   get '/checklist' => 'static#checklist'
   resources :subscriptions, only: [:create]
 
-  get '/resources' => 'static#resources'
+  get '/resources/search' => 'resources#new'
+  post '/resources/search' => 'resources#search'
+  get '/resources/search/:id' => 'resources#results'
+  get '/resources/:id' => 'resources#show'
 
   comfy_route :cms_admin, :path => '/cms'
   comfy_route :cms, :path => '/', :sitemap => false # Make sure this routeset is defined last
