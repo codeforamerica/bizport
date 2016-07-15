118d547529bddb1eda720a0f2423460459cf03f8
Mark Rossetti <mark.a.rossetti@gmail.com>
2016-07-15 13:54:44 -0700

    Scaffolding UI of search and results views

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,9 +4,12 @@ class ResourcesController < ApplicationController
   API_URL = 'https://longbeachresources.tk/wm-api/'
 
   def new
+    benefit_types_url = URI.parse(API_URL + 'benefit-type/')
     industries_url = URI.parse(API_URL + 'industry/')
     purposes_url = URI.parse(API_URL + 'purpose/')
 
+    @benefit_types = api_response_for(benefit_types_url)
+      .map{ |benefit_types| { id: benefit_types["id"], name: benefit_types["name"] } }
     @industries = api_response_for(industries_url)
       .map{ |industry| { id: industry["id"], name: industry["name"] } }
     @purposes = api_response_for(purposes_url)
@@ -17,16 +20,17 @@ class ResourcesController < ApplicationController
   def search
     url = URI.parse(API_URL + 'search/')
 
-    response = api_response_for(url, 'POST', api_params(params))
-    @api_response = JSON.parse(response.body)
-
-    redirect_to '/resources/search/' + @api_response['id'].to_s
+    @api_response = api_response_for(url, 'POST', api_params(params))
+    if @api_response['id']
+      redirect_to '/resources/search/' + @api_response['id'].to_s
+    else
+      redirect_to '/resources/search/', flash: @api_response
+    end
   end
 
   def results
     search_id = params[:id]
     url = URI.parse(API_URL + 'search/' + search_id + '/')
-
     @search_data = api_response_for(url)
   end
 
@@ -42,22 +46,24 @@ class ResourcesController < ApplicationController
 
   def api_response_for(url, method='GET', params={})
     if method == 'POST'
-      return Net::HTTP.post_form(url, params)
+      res = Net::HTTP.post_form(url, params)
     else
       res = Net::HTTP.get_response(url)
       if res.code == '404'
         raise ActiveRecord::RecordNotFound
       end
 
-      return JSON.parse(res.body)
     end
+
+    return JSON.parse(res.body)
   end
 
   def api_params(params)
     valid_params = [
       'city',
       'state',
-      'industry',
+      'benefit_types',
+      'industries',
       'purposes',
       'personal_investment',
       'small_business',
