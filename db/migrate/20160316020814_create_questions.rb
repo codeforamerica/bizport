5bfdb4419135c452640f37f19be8b64bba4f15f5
Patrick Bentley <pbentley07@gmail.com>
2017-10-01 20:08:33 -0400

    Debugging CreateQuestions migration --Randal

--- db/migrate/20160316020814_create_questions.rb
+++ db/migrate/20160316020814_create_questions.rb
@@ -1,4 +1,4 @@
-class CreateQuestions < ActiveRecord::Migration
+class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
       t.string :question_text
